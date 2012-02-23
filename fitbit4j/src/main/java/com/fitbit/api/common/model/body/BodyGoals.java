8bc44322a4db3522755838b9d7562f11fc1295f5
Kiryl Kazakevich <kiryl.kazakevich@gmail.com>
2012-02-23 22:52:36 +0300

    SITE-10739 - Fitbit4J: Add body fat goal to the Get-Body-Measurements

--- fitbit4j/src/main/java/com/fitbit/api/common/model/body/BodyGoals.java
+++ fitbit4j/src/main/java/com/fitbit/api/common/model/body/BodyGoals.java
@@ -1,20 +1,27 @@
 package com.fitbit.api.common.model.body;
 
-import org.apache.commons.lang.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class BodyGoals {
 
     private Double weight;
+    private Double fat;
 
     public BodyGoals(JSONObject json) throws JSONException {
-        if (StringUtils.isNotBlank(json.optString("weight"))) {
+        if (json.has("weight")) {
             weight = json.getDouble("weight");
+        }
+        if (json.has("fat")) {
+            fat = json.getDouble("fat");
         }
     }
 
     public Double getWeight() {
         return weight;
+    }
+
+    public Double getFat() {
+        return fat;
     }
 }
